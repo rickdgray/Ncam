f1c61cfcdffc9bf26e8f30f2dae03daca884769d
Rick Gray <rickdgray@outlook.com>
2024-06-11 13:03:59 -0500

    grid display

--- NamecheapAutomation/Program.cs
+++ NamecheapAutomation/Program.cs
@@ -1,36 +1,56 @@
 ﻿using NamecheapAutomation;
+using Spectre.Console;
 
 internal class Program
 {
-    private static async Task Main(string[] args)
+    private static async Task Main()
     {
         using var httpClient = new HttpClient();
         var ip = await httpClient.GetStringAsync("https://api.seeip.org");
 
         // sandbox
-        //var api = new NameCheapApi("rickdgray", "rickdgray", "", ip, true);
+        var api = new NameCheapApi("rickdgray", "rickdgray", "", ip, true);
 
         // prod
-        var api = new NameCheapApi("rickdgray", "rickdgray", "", ip);
-
-        var hostEntries = new[]
-        {
-            new HostEntry { HostName = "@", RecordType = RecordType.ALIAS, Address = "www.rickdgray.com." },
-            new HostEntry { HostName = "@", RecordType = RecordType.CAA, Address = "0 issue \"letsencrypt.org\"" },
-            new HostEntry { HostName = "www", RecordType = RecordType.CNAME, Address = "rickdgray.github.io." }
-        };
-
-        await api.SetHostsAsync("rickdgray", "com", hostEntries);
+        //var api = new NameCheapApi("rickdgray", "rickdgray", "", ip);
 
         var response = await api.GetHostsAsync("rickdgray", "com");
+        var hosts = response.HostEntries;
 
-        foreach (var host in response.HostEntries)
+        var grid = new Grid();
+        grid.AddColumn();
+        grid.AddColumn();
+        grid.AddColumn();
+
+        grid.AddRow([
+            new Text("Host Name", new Style(Color.Green, Color.Black)),
+            new Text("Record Type", new Style(Color.Red, Color.Black)).RightJustified(),
+            new Text("Address", new Style(Color.Blue, Color.Black))
+        ]);
+
+        foreach (var host in hosts)
         {
-            Console.Write(Enum.GetName(typeof(RecordType), host.RecordType) ?? string.Empty);
-            Console.Write(" ");
-            Console.Write(host.HostName);
-            Console.Write(" ");
-            Console.WriteLine(host.Address);
+            grid.AddRow([
+                new Text(host.HostName),
+                new Text(Enum.GetName(typeof(RecordType), host.RecordType) ?? string.Empty).RightJustified(),
+                new Text(host.Address)
+            ]);
         }
+
+        AnsiConsole.Write(grid);
+
+        Console.WriteLine();
+
+        var selectedHost = AnsiConsole.Prompt(
+            new SelectionPrompt<HostEntry>()
+                .Title("Select a host entry:")
+                .EnableSearch()
+                .PageSize(3)
+                .MoreChoicesText("Scroll down to see more hosts...")
+                .AddChoices(hosts)
+                .UseConverter(h => $"{h.HostName} {Enum.GetName(typeof(RecordType), h.RecordType) ?? string.Empty} {h.Address}")
+        );
+
+        AnsiConsole.MarkupLine($"Selected host: [bold]{selectedHost.HostName}[/]");
     }
 }
